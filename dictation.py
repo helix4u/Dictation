102e66c931bb05e8b9c2216eb8b69127bf4016ce
Gille <4317663+helix4u@users.noreply.github.com>
2025-01-31 09:21:44 -0700

    Update dictation.py

--- dictation.py
+++ dictation.py
@@ -30,7 +30,6 @@ def select_audio_input_device():
         if device_info['maxInputChannels'] > 0:
             print(f"{i}: {device_info['name']}")
     
-    # Note: The default device index of 6 is set for Nvidia Broadcast on the user's machine.
     # Adjust the default for your use case.
     device_index = int(input("Select the device index for audio input (default is 1): ") or 1)
     p.terminate()
